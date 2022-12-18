9b11b814ef8d623a4047c136177b8f5530fc5eb2
strawberrys <56797207+strawbberrys@users.noreply.github.com>
2022-12-18 14:46:36 -0500

    add Page:addSameLine method, update Page:addTextBox
    adds a RenderSameLine which inherits the Page class
    Page:addTextBox now won't create a button if a callback is not supplied

--- ui.lua
+++ ui.lua
@@ -5,105 +5,123 @@ do
     Ui.__index = Ui
     Page.__index = Page
 
-    function Ui.new(title: string, minSize: Vector2?, maxSize: Vector2?)
-        local renderWindow = RenderWindow.new(title)
+    -- Ui class
+    do
+        function Ui.new(title: string, minSize: Vector2?, maxSize: Vector2?)
+            local renderWindow = RenderWindow.new(title)
 
-        if minSize then
-            renderWindow.MinSize = minSize
+            if minSize then
+                renderWindow.MinSize = minSize
+            end
+
+            if maxSize then
+                renderWindow.MaxSize = maxSize
+            end
+
+            renderWindow.VisibilityOverride = true
+            
+            local tabMenu = renderWindow:TabMenu()
+
+            return setmetatable({
+                renderWindow = renderWindow,
+                tabMenu = tabMenu
+            }, Ui)
         end
 
-        if maxSize then
-            renderWindow.MaxSize = maxSize
+        function Ui:addPage(name: string)
+            local tabMenu = self.tabMenu
+            local page = tabMenu:Add(name)
+
+            return Page.new(page)
+        end
+    end
+
+    -- Page class
+    do
+        function Page.new(page: RenderDummyWindow)
+            return setmetatable({page = page}, Page)
         end
 
-        renderWindow.VisibilityOverride = true
-        
-        local tabMenu = renderWindow:TabMenu()
+        function Page:addDivider(name: string)
+            local page = self.page
 
-        return setmetatable({
-            renderWindow = renderWindow,
-            tabMenu = tabMenu
-        }, Ui)
-    end
+            page:Label(name)
+            page:Separator()
+        end
 
-    function Ui:addPage(name: string)
-        local tabMenu = self.tabMenu
-        local page = tabMenu:Add(name)
-
-        return Page.new(page)
-    end
-
-    function Page.new(page: RenderDummyWindow)
-        return setmetatable({page = page}, Page)
-    end
-
-    function Page:addDivider(name: string)
-        local page = self.page
-
-        page:Label(name)
-        page:Separator()
-    end
-
-    function Page:addButton(name: string, callback: () -> ()): RenderButton
-        local button = self.page:Button()
-
-        button.Label = name
-        button.OnUpdated:Connect(callback)
-
-        return button
-    end
-
-    function Page:addToggle(name: string, callback: (enabled: boolean) -> ()): RenderCheckbox
-        local checkbox = self.page:CheckBox()
-
-        checkbox.Label = name
-        checkbox.OnUpdated:Connect(callback)
-
-        return checkbox
-    end
-
-    function Page:addSlider(name: string, min: number, max: number, callback: (value: number) -> ()): RenderIntSlider
-        local intSlider = self.page:IntSlider()
-
-        intSlider.Label = name
-        intSlider.Min = min
-        intSlider.Max = max
-
-        return intSlider
-    end
-
-    function Page:addDropdown(name: string, items: {string}, callback: (newSelection: any) -> ()?): (RenderCombo, RenderButton?)
-        local sameLine = self.page:SameLine()
-        local combo = sameLine:Combo()
-        local button = nil
-
-        combo.Items = items
-
-        if callback then
-            button = sameLine:Button()
+        function Page:addButton(name: string, callback: () -> ()): RenderButton
+            local button = self.page:Button()
 
             button.Label = name
-            button.OnUpdated:Connect(function()
-                callback(combo.Items[combo.SelectedItem])
-            end)
-        else
-            combo.Label = name
+            button.OnUpdated:Connect(callback)
+
+            return button
         end
 
-        return combo, button
-    end
+        function Page:addToggle(name: string, callback: (enabled: boolean) -> ()): RenderCheckbox
+            local checkbox = self.page:CheckBox()
 
-    function Page:addTextBox(name: string, callback: (value: string) -> ()): (RenderTextbox, RenderButton)
-        local sameLine = self.page:SameLine()
-        local textBox = sameLine:TextBox()
-        local button = sameLine:Button()
+            checkbox.Label = name
+            checkbox.OnUpdated:Connect(callback)
 
-        button.Label = name
-        button.OnUpdated:Connect(function()
-            callback(textBox.Value)
-        end)
+            return checkbox
+        end
 
-        return textBox, button
+        function Page:addSlider(name: string, min: number, max: number, callback: (value: number) -> ()): RenderIntSlider
+            local intSlider = self.page:IntSlider()
+
+            intSlider.Label = name
+            intSlider.Min = min
+            intSlider.Max = max
+
+            return intSlider
+        end
+
+        function Page:addDropdown(name: string, items: {string}, callback: (newSelection: any) -> ()?): (RenderCombo, RenderButton?)
+            local sameLine = self.page:SameLine()
+            local combo = sameLine:Combo()
+            local button = nil
+
+            combo.Items = items
+
+            if callback then
+                button = sameLine:Button()
+
+                button.Label = name
+                button.OnUpdated:Connect(function()
+                    callback(combo.Items[combo.SelectedItem])
+                end)
+            else
+                combo.Label = name
+            end
+
+            return combo, button
+        end
+
+        function Page:addTextBox(name: string, callback: (value: string) -> ()?): (RenderTextbox, RenderButton?)
+            local sameLine = self.page:SameLine()
+            local textBox = sameLine:TextBox()
+            local button = nil
+
+            if callback then
+                button = sameLine:Button()
+
+                button.Label = name
+                button.OnUpdated:Connect(function()
+                    callback(textBox.Value)
+                end)
+            else
+                textBox.Label = name
+            end
+
+            return textBox, button
+        end
+
+        function Page:addSameLine()
+            local sameLine = self.page:SameLine()
+
+            return setmetatable({page = sameLine}, Page)
+        end
     end
 end
 
